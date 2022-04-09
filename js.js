7245e5ea7c35e3a2e4eed1a22f55f16f4fdf7a38
Nahuel Fiocco <nahuelfiocco@hotmail.com>
2022-04-09 20:23:11 -0300

    adaptacion de footer

--- js.js
+++ js.js
@@ -13,11 +13,3 @@ if (document.body.classList.contains('dark-mode') {
     } else {
         document.body.classList.remove(toggleDarkMode());
     }*/
-//textarea auto sizing cuando tipeo, como una especie de overflow pero mas pro jeje
-const growers = document.querySelectorAll(".grow-wrap");
-growers.forEach((grower) => {
-    const textarea = grower.querySelector("textarea");
-    textarea.addEventListener("input", () => {
-        grower.dataset.replicatedValue = textarea.value;
-    });
-});
