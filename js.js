c0be87987012144e535b1687431fd9d981ee583d
Nahuel Fiocco <nahuelfiocco@hotmail.com>
2022-04-11 17:17:05 -0300

    remaster

--- js.js
+++ js.js
@@ -1,15 +1,4 @@
-//dark mode
+//dark mode sin local storage
 function toggleDarkMode() {
     document.getElementById("body").classList.toggle("dark-mode");
 }
-/*localStorage para tener darkMode en todos los sitios, no pude hacer que funcione jeje
-if (document.body.classList.contains('dark-mode') {
-        localStorage.setItem('dark-mode-boolean', 'true');
-    } else {
-        localStorage.setItem('dark-mode-boolean', 'false');
-    }
-    if (localStorage.getItem('dark-mode-boolean') === 'true') {
-        document.body.classList.add(toggleDarkMode());
-    } else {
-        document.body.classList.remove(toggleDarkMode());
-    }*/
