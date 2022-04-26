e2df2d5e1cdcf84381c338413692439437eacd00
Nahuel Fiocco <nahuelfiocco@hotmail.com>
2022-04-26 08:52:22 -0300

    introduccion a sass

--- js.js
+++ js.js
@@ -1,16 +1,5 @@
 //dark mode con local storage, no puedo hacer que se mantenga cuando refresco o cuando navego en los sitios ;(
 
 function toggleDarkMode() {
-    if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('dark-mode', 'false');
-        document.body.classList.remove("dark-mode");
-    } else {
-        document.body.classList.add("dark-mode");
-        localStorage.setItem('dark-mode', 'true');
-    }
-}
-if (localStorage.getItem('dark-mode') == 'true') {
-    document.body.classList.add("dark-mode");
-} else {
-    document.body.classList.remove("dark-mode");
+    document.body.classList.toggle('dark-mode');
 }
